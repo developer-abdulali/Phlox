8a3abd5a44db6baa293e76ce17e339a9ddcb8743
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-04 02:56:54 +0500

    added changes

--- components/TeamMembers/TeamMembers.jsx
+++ components/TeamMembers/TeamMembers.jsx
@@ -11,21 +11,21 @@ const TeamMembers = () => {
           <div className="bg-red-500 p-1 w-14"></div>
         </div>
         <h2 className="text-5xl mt-10">Our Team Is Very <br /> Expert To Help</h2>
-        <p className="text-xl mt-7">
+        <p className="text-xl mt-7 text-[#505050d4]">
           A wonderful serenity has taken possession of my entire soul, <br /> like
           these sweet mornings.
         </p>
         <div className="mt-10">
         <Link
             href="/"
-            className="text-white bg-red-500 mt-10 py-4 px-14 rounded-lg hover:bg-white hover:text-red-500 duration-300 cursor-pointer"
+            className="text-white bg-red-500 mt-10 py-4 px-14 rounded-lg hover:bg-white hover:text-red-500 hover:border-2 duration-300 cursor-pointer"
           >
             About
         </Link>
         </div>
       </div>
       <div className="flex gap-5">
-        <Image src="/images/teamMember1.jpeg" alt="Team Member" width={300} height={100}/>
+        <Image src="/images/teamMember1.jpeg" alt="Team Member" width={300} height={100} className="rounded-xl"/>
         <Image src="/images/teamMember2.jpeg" alt="Team Member" width={300} height={100}/>
         <Image src="/images/teamMember3.jpeg" alt="Team Member" width={300} height={100}/>
       </div>
